2ae61de950eb1d1160047c393b0b94ad733089ed
atomsfat <atomsmail@gmail.com>
2015-03-12 23:22:10 -0600

    generating readme.md and package.json

--- test/jsClient-spec.js
+++ test/jsClient-spec.js
@@ -9,7 +9,11 @@ describe('Must run on raml2code', function () {
   var gatitosAPI = function(done){
     test(done, "index.raml" ,generator, {}, "javascript/fixtureApiClient.js", "v1/fixtureApiClient.js")
   };
+  var readme = function(done){
+    test(done, "index.raml" ,generator ,{} , undefined, "README.md", false)
+  };
 
-  it('should generate a js client from RAML file', gatitosAPI );
+  it('should generate a js client from RAML file', gatitosAPI);
+  it('should generate a js client from RAML file', readme);
 
 });
